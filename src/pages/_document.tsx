199feac53586931800c0c3f2467083b3a022b272
Jarles <jarlesjrlz@gmail.com>
2025-08-20 09:45:58 -0300

    alterações

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="es"> {/* ou "es-AR" se for o caso */}
       <Head>
+        <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        {/* use crossOrigin vazio ou "anonymous" (não "true") */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
           rel="stylesheet"
